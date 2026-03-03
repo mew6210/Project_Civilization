eb0aafc973754c9b5cc3fe8202190f600ea145e5
mew6210 <mew.62@wp.pl>
2026-03-03 20:13:13 +0100

    Add project files.

---
+++ main.cpp
@@ -0,0 +1,13 @@
+﻿#include <SFML/Window.hpp>
+
+int main(){
+    sf::Window window(sf::VideoMode({ 800, 600 }), "My window");
+
+    while (window.isOpen()){
+        while (const std::optional event = window.pollEvent()){
+            if (event->is<sf::Event::Closed>())
+                window.close();
+        }
+        
+    }
+}
