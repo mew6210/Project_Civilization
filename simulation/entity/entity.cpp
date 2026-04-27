0ec0e01e125a38a56114bc8ab6b384f754c8b228
Necronelele <kuba.twardowski@spoko.pl>
2026-04-27 23:27:25 +0200

    Adding entity and types
    - Adding entities using keyboard and mouse (1 none, 2 human, 3 tree, 4 bush) (mouse left click)
    - Different types of entities (human, tree, bush) and different colors
    - Creating all of those things directly on cursor placement and bigger number of entities at one time

--- simulation/entity/entity.cpp
+++ simulation/entity/entity.cpp
@@ -2,22 +2,32 @@
 #include <SFML/Graphics/RectangleShape.hpp>
 
 /*
-	Currently renders a 1x1 guy as a 4x4 red square
+	Currently renders a 1x1 entity based on a type as a 4x4 square
 
 	TODO: maybe should be changed later to 1x1, when zoom is implemented
 */
 void Entity::render(sf::RenderWindow& window) const {
 	sf::RectangleShape shape{ sf::Vector2f{4,4} };
 	shape.setPosition(sf::Vector2f{ float(m_entState.m_posX),float(m_entState.m_posY) });
-	shape.setFillColor(sf::Color{ 255,0,0 });
-	shape.setOutlineColor(sf::Color{ 255,0,0 });
+
+	switch (m_type) {
+		case ActiveTool::Human:  shape.setFillColor(sf::Color::Red); break;
+		case ActiveTool::Tree:   shape.setFillColor(sf::Color(139, 69, 19)); break;
+		case ActiveTool::Bush:   shape.setFillColor(sf::Color::Magenta); break;
+		default:                 shape.setFillColor(sf::Color::White); break;
+	}
+
 	window.draw(shape);
 }
 
 void Entity::sim() {
-	if (m_tasks.empty()) {
-		m_tasks.push_back(std::make_unique<WanderRandTask>(m_entState));
-	}
+	if (m_type == ActiveTool::Human) {
+		if (m_tasks.empty()) {
+			m_tasks.push_back(std::make_unique<WanderRandTask>(m_entState));
+		}
 
-	m_tasks[m_curTask]->tick();
+		if (m_curTask < m_tasks.size()) {
+			m_tasks[m_curTask]->tick();
+		}
+	}
 }
---
+++ simulation/type.hpp
@@ -0,0 +1,8 @@
+#pragma once
+
+enum class ActiveTool {
+	None,
+	Human,
+	Tree,
+	Bush,
+};
