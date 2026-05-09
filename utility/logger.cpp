abe069674327d9ac7fe6921adb5a0af5d4b3fade
Necronelele <kuba.twardowski@spoko.pl>
2026-05-09 21:05:49 +0200

    Logger v2
    - Implemented color usage for linux and macos and if it won't work for some reason the log message won't have any weird things just plain white text.
    - now only type of log (in [] brackets) is in colour and the rest of the log is plain white
    - added a folder for log.txt now the path is: Project_Civilization\out\build\x64-debug\logs/log.txt.
    - now error log only shows the log message not some aditional text messasge.

--- utility/logger.cpp
+++ utility/logger.cpp
@@ -4,30 +4,40 @@
 #include <windows.h>
 #endif
 
-Logger::Logger(const std::string& filename) {
-    fileStream.open(filename, std::ios::app);
-    setupConsole();
+Logger::Logger(const std::string& path) {
+    std::filesystem::path logPath(path);
+    if (logPath.has_parent_path()) {
+        std::filesystem::create_directories(logPath.parent_path());
+    }
+
+    fileStream.open(path, std::ios::app);
+
+    //Checking if terminal supports colors
+    useColors = setupConsole();
 }
 
 Logger::~Logger() {
-    if (fileStream.is_open()) {
-        fileStream.close();
-    }
+    if (fileStream.is_open()) fileStream.close();
 }
 
-void Logger::setupConsole() {
-    //Enabling Virtual Terminal Processing
+bool Logger::setupConsole() {
 #ifdef _WIN32
     HANDLE hOut = GetStdHandle(STD_OUTPUT_HANDLE);
-    if (hOut != INVALID_HANDLE_VALUE) {
-        DWORD dwMode = 0;
-        if (GetConsoleMode(hOut, &dwMode)) {
-            dwMode |= 0x0004;
-            SetConsoleMode(hOut, dwMode);
-        }
+    if (hOut == INVALID_HANDLE_VALUE) return false;
+
+    DWORD dwMode = 0;
+    if (!GetConsoleMode(hOut, &dwMode)) return false;
+
+    dwMode |= ENABLE_VIRTUAL_TERMINAL_PROCESSING;
+    if (!SetConsoleMode(hOut, dwMode)) {
+        return false; //Returns false if colors can't be turned on
     }
+    return true;
+#else
+    //For Linux/MacOS if terminal is TTY
+    return isatty(STDOUT_FILENO);
 #endif
 }
 
 //Global variable
-Logger defaultLogger = Logger("log.txt");
+Logger defaultLogger = Logger("logs/log.txt");
--- utility/logger.hpp
+++ utility/logger.hpp
@@ -3,6 +3,7 @@
 #include <string>
 #include <fstream>
 #include <utility>
+#include <filesystem>
 
 enum class LogLevel {
     SUCCESS,
@@ -13,7 +14,7 @@ enum class LogLevel {
 
 class Logger {
 public:
-    Logger(const std::string& filename = "log.txt");
+    Logger(const std::string& filename = "logs/log.txt");
     ~Logger();
 
     //Errorlog
@@ -21,7 +22,6 @@ public:
     void errorLog(bool crash, Args&&... args) {
         logInternal(LogLevel::ERROR, std::forward<Args>(args)...);
         if (crash) {
-            std::cerr << "\033[31m!!! FATAL ERROR: Program terminated !!!\033[0m" << std::endl;
             std::exit(EXIT_FAILURE);
         }
     }
@@ -44,32 +44,38 @@ public:
 
 private:
     std::ofstream fileStream;
+    bool useColors = false;
 
     //Function that colors and puts text together
     template<typename... Args>
     void logInternal(LogLevel level, Args&&... args) {
-        std::string prefix;
+        std::string symbol;
         std::string colorCode;
 
         switch (level) {
-        case LogLevel::SUCCESS: prefix = "[+] "; colorCode = "\033[32m"; break; //Green
-        case LogLevel::WARNING: prefix = "[?] "; colorCode = "\033[33m"; break; //Yellow
-        case LogLevel::ERROR:   prefix = "[-] "; colorCode = "\033[31m"; break; //Red
-        case LogLevel::INFO:    prefix = "[i] "; colorCode = "\033[36m"; break; //Turquoise
+        case LogLevel::SUCCESS: symbol = "+"; colorCode = "\033[32m"; break;
+        case LogLevel::WARNING: symbol = "?"; colorCode = "\033[33m"; break;
+        case LogLevel::ERROR:   symbol = "-"; colorCode = "\033[31m"; break;
+        case LogLevel::INFO:    symbol = "i"; colorCode = "\033[36m"; break;
         }
 
-        std::cout << colorCode << prefix;
+        std::cout << "[";
+        if (useColors) std::cout << colorCode;
+        std::cout << symbol;
+        if (useColors) std::cout << "\033[0m";
+        std::cout << "] ";
+
         (std::cout << ... << std::forward<Args>(args));
-        std::cout << "\033[0m" << std::endl;
+        std::cout << std::endl;
 
         if (fileStream.is_open()) {
-            fileStream << prefix;
+            fileStream << "[ " << symbol << " ] ";
             (fileStream << ... << std::forward<Args>(args));
             fileStream << std::endl;
         }
     }
 
-    void setupConsole();
+    bool setupConsole();
 };
 
 extern Logger defaultLogger;
